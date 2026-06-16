848af536c9a00260affbfc78bf885295d57b2233
Junicoders <166041073+junicoders@users.noreply.github.com>
2026-06-16 15:42:02 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-// keyestudio Infrared Wireless Module Kit for microbit
+// Infrared Wireless Module Kit for microbit
 // (receiver module+remote controller)
 // author: jieliang mo
 // github:https://github.com/mworkfun
-// Write the date: 2020-5-15
+// Write the date: 2020-6-19
 
 const enum IrButton {
     //% block=" "
@@ -49,15 +49,15 @@ const enum IrButton {
     Hash = 74
 }
 /**
- * create IR_receiver namespace
- * use for keyestudio IR receiver and IR emission kit
+ * criar namespace ControloIV
+ * usar para recetor IV e kit de emissão IV
  * author: jieliang mo
- * Write the date: 2020-5-15
+ * Write the date: 2020-6-19
  */
 //% color="#ff6800" weight=10 icon="\uf1eb"
 namespace ControloIV {
     /**
-     * define a IR receiver class
+     * definir uma classe de recetor IV
      */
     class irReceiver {
         constructor() {
@@ -68,17 +68,17 @@ namespace ControloIV {
         command: number;
         IR_pin: DigitalPin;
     }
-    //create a IR receiver class
+    //criar uma classe de recetor IV
     let IR_R = new irReceiver;
 
-    //define nec_IR maximum number of pulses is 33.
-    //create 2 pulse cache array.
+    //definir o número máximo de pulsos nec_IR como 33.
+    //criar 2 arrays de cache de pulsos.
     let maxPulse: number = 33;
     let low_pulse: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     let high_pulse: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-    //must define for 33,
-    //otherwise there is a risk of error in receiving the first data.
+    //deve ser definido como 33,
+    //caso contrário existe risco de erro ao receber os primeiros dados.
     let LpulseCounter: number = 33;
     let HpulseCounter: number = 33;
 
@@ -86,16 +86,16 @@ namespace ControloIV {
     let HpulseTime: number = 0;
     //let pulse9ms: boolean = false;
     //let pulse4ms: boolean = false;
-    //This variable will become true when the pulse is repeated
+    //Esta variável torna-se verdadeira quando o pulso é repetido
     //let repeatedPulse: boolean = false;
 
     /**
-     * initialize the IR receiver function
+     * inicializar a função do recetor IV
      */
     function irInit(IR_pin: DigitalPin) {
-        pins.onPulsed(IR_pin, PulseValue.Low, () => {      //interrupt event
-            LpulseTime = pins.pulseDuration();             //measure the pulse
-            if (8250 < LpulseTime && LpulseTime < 9250) {  //9ms
+        pins.onPulsed(IR_pin, PulseValue.Low, () => {      //evento de interrupção
+            LpulseTime = pins.pulseDuration();             //medir o pulso
+            if (6750 < LpulseTime && LpulseTime < 11250) { //9ms
                 LpulseCounter = 0;
             }
             if (LpulseCounter < maxPulse /*&& repeatedPulse == false*/) {
@@ -105,10 +105,10 @@ namespace ControloIV {
         });
         pins.onPulsed(IR_pin, PulseValue.High, () => {
             HpulseTime = pins.pulseDuration();
-            /*if (2000 < HpulseTime && HpulseTime < 2500) {  //2.25ms
+            /*if (1687 < HpulseTime && HpulseTime < 2812) {  //2.25ms
                 repeatedPulse = true;
             }*/
-            if (4250 < HpulseTime && HpulseTime < 4750) {  //4.5ms
+            if (3375 < HpulseTime && HpulseTime < 5625) {    //4.5ms
                 HpulseCounter = 0;
                 //repeatedPulse = false;
             }
@@ -119,10 +119,10 @@ namespace ControloIV {
         });
     }
     /**
-    * Convert the pulse into data function
+    * Função de conversão de pulso em dados
     * author: jieliang mo
     * github:https://github.com/mworkfun
-    * Write the date: 2020-5-15
+    * Write the date: 2020-6-19
     */
     function irDataProcessing() {
         let tempAddress: number = 0;
@@ -130,34 +130,34 @@ namespace ControloIV {
         let tempCommand: number = 0;
         let inverseCommand: number = 0;
         let num: number;
-        //confirm start pulse
-        if (8250 < low_pulse[0] && low_pulse[0] < 9250 && HpulseCounter >= 33) {
-            //conver the pulse into data
+        //confirmar pulso de início
+        if (6750 < low_pulse[0] && low_pulse[0] < 11250 && HpulseCounter >= 33) {  //9ms
+            //converter o pulso em dados
             for (num = 1; num < maxPulse; num++) {
-                //if (440 < low_pulse[num] && low_pulse[num] < 680) {      //0.56ms
-                if (1400 < high_pulse[num] && high_pulse[num] < 2000) {  //1.69ms = 1, 0.56ms = 0
-                    if (1 <= num && num < 9) {    //conver the pulse into address
+                //if (420 < low_pulse[num] && low_pulse[num] < 700) {      //0.56ms
+                if (1267 < high_pulse[num] && high_pulse[num] < 2112) {    //1.69ms = 1, 0.56ms = 0
+                    if (1 <= num && num < 9) {    //converter o pulso em endereço
                         tempAddress |= 1 << (num - 1);
                     }
-                    if (9 <= num && num < 17) {   //conver the pulse into inverse address
+                    if (9 <= num && num < 17) {   //converter o pulso em endereço inverso
                         inverseAddress |= 1 << (num - 9);
                     }
-                    if (17 <= num && num < 25) {   //conver the pulse into command
+                    if (17 <= num && num < 25) {   //converter o pulso em comando
                         tempCommand |= 1 << (num - 17);
                     }
-                    if (25 <= num && num < 33) {   //conver the pulse into inverse command
+                    if (25 <= num && num < 33) {   //converter o pulso em comando inverso
                         inverseCommand |= 1 << (num - 25);
                     }
                 }
                 //}
             }
             low_pulse[0] = 0;
-            //check the data and return the data to IR receiver class.
+            //verificar os dados e devolvê-los à classe do recetor IV.
             if ((tempAddress + inverseAddress == 0xff) && (tempCommand + inverseCommand == 0xff)) {
                 IR_R.address = tempAddress;
                 IR_R.command = tempCommand;
                 return;
-            } else {  //Return -1 if check error.
+            } else {  //Devolver -1 em caso de erro de verificação.
                 IR_R.address = -1;
                 IR_R.command = -1;
                 return;
@@ -167,10 +167,10 @@ namespace ControloIV {
         IR_R.command = 0;
     }
     /**
-     * Connects to the IR receiver module at the specified pin.
+     * Liga ao módulo recetor IV no pino especificado.
      * author: jieliang mo
      * github:https://github.com/mworkfun
-     * Write the date: 2020-5-15
+     * Write the date: 2020-6-19
      */
     //% blockId="infrared_connect"
     //% block="conectar o recetor IV no %IR_pin"
@@ -179,15 +179,15 @@ namespace ControloIV {
     //% IR_pin.fieldOptions.tooltips="false"
     //% weight=99
     export function connectInfrared(IR_pin: DigitalPin): void {
-        IR_R.IR_pin = IR_pin;   //define IR receiver control pin
+        IR_R.IR_pin = IR_pin;   //definir o pino de controlo do recetor IV
         pins.setPull(IR_R.IR_pin, PinPullMode.PullUp);
-        irInit(IR_R.IR_pin);   //initialize the IR receiver
+        irInit(IR_R.IR_pin);   //inicializar o recetor IV
     }
     /**
-     * Returns the command code of a specific IR button.
+     * Devolve o código de comando de um botão IV específico.
      * author: jieliang mo
      * github:https://github.com/mworkfun
-     * Write the date: 2020-5-15
+     * Write the date: 2020-6-19
      */
     //% blockId=infrared_button
     //% button.fieldEditor="gridpicker"
@@ -199,18 +199,18 @@ namespace ControloIV {
         return button as number;
     }
     /**
-     * Returns the code of the IR button that is currently pressed and 0 if no button is pressed.
-     * It is recommended to delay 110ms to read the data once
+     * Devolve o código do botão IV atualmente premido e 0 se nenhum botão estiver premido.
+     * Recomenda-se um atraso de 110ms para ler os dados uma vez
      * author: jieliang mo
      * github:https://github.com/mworkfun
-     * Write the date: 2020-5-18
+     * Write the date: 2020-6-19
      */
     //% blockId=infrared_pressed_button
     //% block="Botão IV"
     //% weight=97
     export function returnIrButton(): number {
         irDataProcessing();
+        basic.pause(80);      //Atraso de um período de receção infravermelha
         return IR_R.command;
     }
 }
-
